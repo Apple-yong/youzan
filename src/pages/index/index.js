2de60dcb513f8a6cbfb5a7b671c6b670fd81a0c0
liyong <748840808@qq.com>
2020-03-02 23:37:55 +0800

    下拉列表渲染

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -5,18 +5,45 @@ import Vue from 'vue'
 import axios from 'axios'
 import url from 'js/api.js'
 
+import { InfiniteScroll } from 'mint-ui'
+Vue.use(InfiniteScroll)
+
 let app = new Vue({
   el: '#app',
   data: {
-    lists: null
+    lists: null,
+    pageNum: 1,
+    pageSize: 6,
+    loading: false,
+    allLoaded: false
   },
   created () {
-    axios.get(url.hotLists, {
-      pageNum: 1,
-      pageSize: 6
-    }).then(res => {
-      this.lists = res.data.lists
-    })
+    this.getLists()
+  },
+  methods: {
+    getLists () {
+      if (this.allLoaded) return
+      this.loading = true
+      axios.get(url.hotLists, {
+        pageNum: this.pageNum,
+        pageSize: this.pageSize
+      }).then(res => {
+        let curLists = res.data.lists
+        // 判断所有数据是否加载完毕
+        if (curLists.length < this.pageSize) {
+          this.allLoaded = true
+        }
+        if (this.lists) {
+          // 拼接到一起
+          this.lists = this.lists.concat(curLists)
+        } else {
+          // 第一次请求数据初始化
+          this.lists = curLists
+        }
+        this.loading = false
+        this.pageNum++
+      })
+    }
   }
 })
 
