715c6bce3ebf448c7317f113d51046444390b1e1
Apple-yong <748840808@qq.com>
2020-03-11 18:23:38 +0800

    添加购物车

--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -1,3 +1,16 @@
 import './cart.css'
 import './cart_base.css'
 import './cart_trade.css'
+
+import Mock from 'mockjs'
+let Random = Mock.Random
+
+let data = Mock.mock({
+    'list|1-10': [{
+        // 属性 id 是一个自增数，起始值为 1，每次增 1
+        'id|+1': 1
+    }]
+})
+
+
+console.log(JSON.stringify(data, null, 4))
