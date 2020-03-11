82e5f0c3d237a10f19b62282f18a0136042a1bb7
Apple-yong <748840808@qq.com>
2020-03-11 16:30:56 +0800

    购物车弹窗动画以及内部数据处理

--- src/pages/goods/goods.js
+++ src/pages/goods/goods.js
@@ -5,6 +5,7 @@ import './goods_custom.css'
 import './goods_mars.css'
 import './goods_sku.css'
 import './goods_theme.css'
+import './goods_transition.css'
 
 import Vue from 'vue'
 import axios from 'axios'
@@ -24,7 +25,10 @@ new Vue({
         detailTab,
         tabIndex: 0,
         dealLists: null,
-        bannerLists: null
+        bannerLists: null,
+        skuType: 1,
+        showSku: false,
+        skuNum: 1
     },
     methods: {
         getDetails(){
@@ -51,13 +55,30 @@ new Vue({
             .then(res => {
                 this.dealLists = res.data.data.lists
             })
-        }
+        },
+        chooseSku(type){
+            this.skuType = type
+            this.showSku = true
+        },
+        changeSkuNum(num){
+            if(this.skuNum === 1 && num<0) return
+            this.skuNum = this.skuNum + num
+        },
     },
     created(){
         this.getDetails()
     },
     components: {
       swipe
+    },
+    watch: {
+        // 监听阴影框是否打开，打开就关闭滚动
+        showSku(newVal, oldVal){
+            document.body.style.overflow = newVal ? 'hidden' : 'auto'
+            document.querySelector('html').style.overflow = newVal ? 'hidden' : 'auto'
+            document.body.style.height = newVal ? '100%' : 'auto'
+            document.querySelector('html').style.height = newVal ? '100%' : 'auto'
+        }
     },
     mixins: [mixin]
 })
