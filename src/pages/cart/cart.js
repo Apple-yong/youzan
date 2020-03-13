456ec722f862a632a6c851e28aafbbc38982c044
Apple-yong <748840808@qq.com>
2020-03-13 18:39:23 +0800

    购物车删除

--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -12,7 +12,12 @@ new Vue({
     el:'.container',
     data: {
         lists: null,
-        totalPrice: 0
+        totalPrice: 0,
+        editingShop: null,
+        editingShopIndex: -1,
+        // 是否进行删除
+        removePopup: false,
+        removeData: null
     },
     created(){
         this.getList()
@@ -37,6 +42,22 @@ new Vue({
                 });
             }
         },
+        allRemoveSelected: {
+            get() {
+                if(this.editingShop){
+                    return this.editingShop.removeChecked
+                }
+                return false
+            },
+            set(newVal) {
+                if(this.editingShop){
+                    this.editingShop.removeChecked = newVal
+                    this.editingShop.goodsList.forEach(good => {
+                        good.removeChecked = newVal
+                    })
+                }
+            }
+        },
         selectLists() {
             if(this.lists && this.lists.length){
                 let arr = []
@@ -53,6 +74,18 @@ new Vue({
                 return arr
             }
             return []
+        },
+        removeLists() {
+            if(this.editingShop){
+               let arr = []
+               this.editingShop.goodsList.forEach(good => {
+                    if(good.removeChecked){
+                        arr.push(good)
+                    }
+               })
+               return arr
+            }
+            return []
         }
     },
     methods: {
@@ -61,9 +94,14 @@ new Vue({
                 // 先处理，再赋值，响应式原理
                 let lists = res.data.cartList
                 lists.forEach(shop => {
-                    shop.checked = true
+                    // 初始状态
+                    shop.checked = false
+                    shop.removeChecked = false
+                    shop.editing = false
+                    shop.editingMsg = '编辑'
                     shop.goodsList.forEach(good => {
-                        good.checked = true
+                        good.checked = false
+                        good.removeChecked = false
                     })
                 })
                 this.lists = lists
@@ -71,21 +109,84 @@ new Vue({
         },
         // 商品选择
         selectGood(shop,good){
-            good.checked = !good.checked
-            shop.checked = shop.goodsList.every(good => {
-                return good.checked
+            // 判断是否在编辑状态，编辑是removeChecked，正常是checked
+            let attr = this.editingShop ? 'removeChecked' : 'checked'
+            good[attr] = !good[attr]
+            shop[attr] = shop.goodsList.every(good => {
+                return good[attr]
             })
         },
         // 店铺选择后里面商品全选
         selectShop(shop){
-            shop.checked = !shop.checked
+            // 判断是否在编辑状态，编辑是removeChecked，正常是checked
+            let attr = this.editingShop ? 'removeChecked' : 'checked'
+            shop[attr] = !shop[attr]
             shop.goodsList.forEach(good => {
-                good.checked = shop.checked
+                good[attr] = shop[attr]
             })
         },
         // 全选
         selectAll(){
-            this.allSelected = !this.allSelected
+            let attr = this.editingShop ? 'allRemoveSelected' : 'allSelected'
+            this[attr] = !this[attr]
+        },
+        edit(shop, shopIndex){
+            shop.editing = !shop.editing
+            shop.editingMsg = shop.editing ? '完成' : '编辑'
+            this.lists.forEach((item,i) => {
+                if(shopIndex !== i){
+                    item.editing = false
+                    item.editingMsg = shop.editing ? '' : '编辑'
+                }
+            })
+            this.editingShop = shop.editing ? shop : null
+            this.editingShopIndex = shop.editing ? shopIndex : -1
+        },
+        reduce(good){
+            if(good.number === 1) return
+            axios.post(url.cartReduce, {
+                id: good.id,
+                number: 1
+            })
+            .then(res => {
+                good.number--
+            })
+        },
+        add(good){
+            axios.post(url.cartAdd, {
+                id: good.id,
+                number: 1
+            })
+            .then(res => {
+                good.number++
+            })
+        },
+        remove(shop,shopIndex,good,goodIndex){
+            this.removePopup = true
+            this.removeData = {shop,shopIndex,good,goodIndex}
+        },
+        removeConfirm(){
+            let {shop,shopIndex,good,goodIndex} = this.removeData
+            axios.post(url.cartRemove,{
+                id: good.id
+            })
+            .then(res => {
+                shop.goodsList.splice(goodIndex, 1)
+                if(!shop.goodsList.length){
+                    this.lists.splice(shopIndex, 1)
+                    this.removeShop()
+                }
+                this.removePopup = false
+            })
+        },
+        // 删除完店铺所有商品，回到编辑状态
+        removeShop() {
+            this.editingShop = null
+            this.editingShopIndex = -1
+            this.lists.forEach(shop => {
+                shop.editingMsg = '编辑'
+                shop.editing = false
+            });
         }
     },
     mixins: [mixin]
