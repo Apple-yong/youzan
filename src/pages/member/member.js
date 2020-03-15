ea149ac5b2c9a0fe667aa61970d8c44d3c8f5769
liyong <748840808@qq.com>
2020-03-16 00:38:15 +0800

    所有页面都加上底部footer

--- src/pages/member/member.js
+++ src/pages/member/member.js
@@ -1,2 +1,24 @@
 import './member.css'
 import './member_base.css'
+
+import Vue from 'vue'
+import axios from 'axios'
+import url from 'js/api.js'
+import mixin from 'js/mixin.js'
+
+new Vue({
+  el: '.container',
+  data: {
+
+  },
+  created(){
+
+  },
+  methods: {
+
+  },
+  computed: {
+
+  },
+  mixins: [mixin]
+})
