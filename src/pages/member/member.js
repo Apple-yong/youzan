079426641a4d819a12201c11d47d617b312e90dd
Apple-yong <748840808@qq.com>
2020-03-17 15:17:25 +0800

    解决Failed to mount component: template or render function not defined

--- src/pages/member/member.js
+++ src/pages/member/member.js
@@ -1,13 +1,12 @@
 // 1、使用vue-router
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import member from './components/member.vue'
 Vue.use(VueRouter)
 
-let routes = [{
-  path: '/',
-  component: require('./components/member.vue')
-}]
+let routes = [
+  { path: '/', component: require('./components/member.vue').default},
+  { path: '/address', component: require('./components/address.vue').default}
+]
 
 // 2、创建vue-router实例
 let router = new VueRouter({
@@ -17,6 +16,6 @@ let router = new VueRouter({
 
 new Vue({
   el: '#app',
-  router,
-  render: h => h(member),
-})
+  router
+
+}).$mount('#app');
