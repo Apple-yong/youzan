8870e76428745a7cb067153cb28182fc730b1318
Apple-yong <748840808@qq.com>
2020-03-18 15:32:39 +0800

    添加、修改、删除、默认的数据用vuex实现

--- src/pages/member/components/form.js
+++ src/pages/member/components/form.js
@@ -28,40 +28,52 @@ export default {
         this.id = ad.id
       }
     },
+    computed: {
+      lists(){
+        return this.$store.state.lists
+      }
+    },
     methods: {
       add() {
         // 校验
         let {name, tel, provinceValue, cityValue, districtValue, address} = this
         let data = {name, tel, provinceValue, cityValue, districtValue, address}
+        if(this.type === 'add'){
+          this.$store.dispatch('addAction',data)
+        }
         if(this.type === 'edit') {
           data.id = this.id
-          axios.post(url.addressUpdate,data)
-          .then(res => {
-            this.$router.go(-1)
-          })
-        }else {
-          axios.post(url.addressAdd,data)
-          .then(res => {
-            this.$router.go(-1)
-          })
+          // axios.post(url.addressUpdate,data)
+          // .then(res => {
+          //   this.$router.go(-1)
+          // })
+          this.$store.dispatch('updateAction',data)
         }
       },
       remove() {
         if (window.confirm("确认删除?")) { 
-            axios.post(url.addressRemove,this.id)
-            .then(res => {
-                this.$router.go(-1)
-            })
+            // axios.post(url.addressRemove,this.id)
+            // .then(res => {
+            //     this.$router.go(-1)
+            // })
+            this.$store.dispatch('removeAction',this.id)
         } 
       },
       setDefault() {
-        axios.post(url.addressSetDefault,this.id)
-        .then(res => {
-            this.$router.go(-1)
-        })
+        // axios.post(url.addressSetDefault,this.id)
+        // .then(res => {
+        //     this.$router.go(-1)
+        // })
+        this.$store.dispatch('setDefaultAction',this.id)
       }
     },
     watch: {
+      lists: {
+        handler() {
+          this.$router.go(-1)  // 对lists数据进行监听，数据变化就跳转
+        },
+        deep: true  // 深度监听
+      },
       provinceValue(val) {  // val是这个省份的value
         if(val === -1) return
         let index = this.addressData.list.findIndex(item => {
--- src/pages/member/vuex/index.js
+++ src/pages/member/vuex/index.js
@@ -15,6 +15,30 @@ export const store = new Vuex.Store({
   mutations: {
     init(state, lists) {
       state.lists = lists
+    },
+    add(state, instance){
+      state.lists.push(instance)
+    },
+    remove(state, id){
+      let lists = state.lists
+      let index = lists.findIndex(item =>{
+        return item.id == id
+      })
+      lists.splice(index,1)
+    },
+    update(state, instance) {
+      let lists = JSON.parse(JSON.stringify(state.lists))
+      let index = lists.findIndex(item =>{
+        return item.id == instance.id
+      })
+      lists[index] = instance
+      state.lists = lists
+    },
+    setDefault(state, id) {
+      let lists = state.lists
+      lists.forEach(item =>{
+        item.isDefault = item.id == id ? true : false
+      })
     }
   },
   // 异步操作的数据，只能调用mutations
@@ -22,7 +46,33 @@ export const store = new Vuex.Store({
     getLists({commit}){
       axios.get(url.addressLists)
       .then(res => {
-        store.commit('init',res.data.lists)
+        commit('init',res.data.lists)
+      })
+    },
+    addAction({commit},instance){
+      axios.post(url.addressAdd, instance)
+      .then(res => {
+        // 模拟添加id,instance最好后台返回
+        instance.id = parseInt(Math.random()*10000)
+        commit('add', instance)
+      })
+    },
+    removeAction({commit}, id){
+      axios.post(url.addressRemove,id)
+      .then(res => {
+        commit('remove', id)
+      })
+    },
+    updateAction({commit},instance){
+      axios.post(url.addressUpdate,instance)
+      .then(res => {
+        commit('update', instance)
+      })
+    },
+    setDefaultAction({commit},id){
+      axios.post(url.addressSetDefault,id)
+      .then(res => {
+        commit('setDefault', id)
       })
     }
   }
