7902b7729f46f6d8e94bf07453904df333ab3932
Apple-yong <748840808@qq.com>
2020-04-01 16:37:40 +0800

    切回http

--- src/modules/js/api.js
+++ src/modules/js/api.js
@@ -21,8 +21,8 @@ let url = {
 }
 // 开发环境和真实环境的切换
 // let host = ''
-// let host = 'http://rap2api.taobao.org/app/mock/7058'
-let host = 'https://www.easy-mock.com/mock/5c9c3045d172204b3a07ecb0/youzan'
+let host = 'http://rap2api.taobao.org/app/mock/7058'
+// let host = 'https://www.easy-mock.com/mock/5c9c3045d172204b3a07ecb0/youzan'
 
 
 for (const key in url) {
